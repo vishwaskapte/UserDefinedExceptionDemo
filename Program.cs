2ce5b1f57e58e7edd1848f4f9646a12c20c38fc8
Vishwas Kapte <vishwasvkapte@outlook.com>
2022-12-07 19:26:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿public class MyNewException : Exception
+{
+    public MyNewException(string ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new MyNewException("User defined exception!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Catching Exception: " + e.Message);
+        }
+        Console.WriteLine("Next statement to be executed");
+        Console.Read();
+    }
+}
